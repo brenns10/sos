f71c6e4f2b94051f7abddf0f94257fab623009ee
Luc Bettaieb <lucbettaieb@users.noreply.github.com>
2018-09-04 13:56:08 -0700

    Added legs
    Untested but it should work

--- go.sh
+++ go.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-arm-none-eabi-as uart.s -o uart.o
-arm-none-eabi-ld -T test.ld uart.o -o test.elf
-arm-none-eabi-objcopy -O binary test.elf test.bin
+
+alias leg-none-eabi-as='arm-none-eabi-as'
+alias leg-none-eabi-ld='arm-none-eabi-ld'
+alias leg-none-eabi-objcopy='arm-none-eabi-objcopy'
+
+leg-none-eabi-as uart.s -o uart.o
+leg-none-eabi-ld -T test.ld uart.o -o test.elf
+leg-none-eabi-objcopy -O binary test.elf test.bin
 qemu-system-arm -M virt -kernel test.bin -nographic
