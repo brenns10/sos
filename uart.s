2e89d5cc9efc8030c01e68e4bce418a79c2532a7
Stephen Brennan <stephen@brennan.io>
2018-09-05 08:46:00 -0700

    It's not working and i don't know why

--- uart.s
+++ uart.s
@@ -1,38 +1,48 @@
 /*
- * Basic memory layout:
- *
- *     0x00000000 - Reset vector
+ * Print to serial on PL011 UART.
  */
 .section .init
 .equ UART_BASE, 0x09000000
 .equ FR, 0x018
 
 .globl main
+.globl _start
+_start:
 main:
-	mov sp, #0x1000
-	mov r0, #'h'
+	mov r0, #'A'
 	bl print
-	mov r0, #'i'
+	ldr r0, =hello_world
+	ldrb r0, [r5]
+	//mov r0, #'H'
 	bl print
-	mov r0, #' '
-	bl print
-	mov r0, #'w'
-	bl print
-	mov r0, #'r'
-	bl print
-	mov r0, #'l'
-	bl print
-	mov r0, #'d'
+	mov r0, #'Z'
 	bl print
 loop:
 	b loop
 
+/* Prints the nul-terminated string pointed by r0 */
+print_string:
+	push {r4, lr}
+	mov r4, r0
+_print_string_loopldr:
+	ldrb r0, [r4]
+	cmp r0, #0
+	beq _print_string_return
+	bl print
+	add r4, #1
+_print_string_return:
+	pop {r4, pc}
+
+/* Prints the byte in r0 */
 print:
-	mov r1, #0x020  /* bit 5 */
+	mov r1, #0x020     /* bit 5 of flag register = TX FIFO Full  */
 	mov r2, #UART_BASE
 _print_wait:
 	ldr r3, [r2, #FR]
-	tst r3, r1
+	tst r3, r1         /* wait until TX FIFO is not full */
 	bne _print_wait
-	str r0, [r2]
+	str r0, [r2]       /* DR has offset 0 */
 	mov pc, lr
+
+.data
+hello_world: .asciz "Hello\n"
