ee60fc34048d316b4b7fb10de7d7f4681535a9fb
Stephen Brennan <stephen@brennan.io>
2018-11-17 13:06:49 -0800

    IT WORKS

--- go.sh
+++ go.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+
+# Stop on first failure
+set -e
+
+# Compile
 arm-none-eabi-as -g uart.s -o uart.o
+
+# Link
 arm-none-eabi-ld -T test.ld uart.o -o test.elf
-#arm-none-eabi-ld uart.o -o test
+
+# Get the binary image
 arm-none-eabi-objcopy -O binary test.elf test.bin
-qemu-system-arm -M virt -kernel test.elf -nographic
-#qemu-system-arm -M virt -kernel test.bin -nographic -gdb tcp::9000 -S
+
+# Run as normal
+#qemu-system-arm -M virt -kernel test.bin -nographic
+
+# Run with gdb
+qemu-system-arm -M virt -kernel test.bin -nographic -gdb tcp::9000 -S
+
+# Run without a "system" - segfaults
+#qemu-arm -singlestep -g 9000 test.elf
--- uart.s
+++ uart.s
@@ -1,19 +1,19 @@
 /*
  * Print to serial on PL011 UART.
  */
-.section .init
-.equ UART_BASE, 0x09000000
-.equ FR, 0x018
+.text
 
-.globl main
 .globl _start
 _start:
-main:
+	/* Set stack pointer to top of stack, grows downward.
+	 * Top of stack is defined in the link script.
+	 */
+	ldr sp, =stack_top
+
 	mov r0, #'A'
 	bl print
-	ldr r5, =hello_world
-	ldrb r0, [r5]
-	bl print
+	ldr r0, =hello_world
+	bl print_string
 	mov r0, #'Z'
 	bl print
 loop:
@@ -34,6 +34,9 @@ _print_string_return:
 	pop {r4, pc}
 
 /* Prints the byte in r0 */
+.equ UART_BASE, 0x09000000
+.equ FR, 0x018
+
 print:
 	mov r1, #0x020     /* bit 5 of flag register = TX FIFO Full  */
 	mov r2, #UART_BASE
@@ -44,4 +47,5 @@ _print_wait:
 	str r0, [r2]       /* DR has offset 0 */
 	mov pc, lr
 
+.data
 hello_world: .asciz "Hello\n"
