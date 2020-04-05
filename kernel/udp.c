49105c179c4a1207002f429bc4238c79d27a11a4
Stephen Brennan <stephen@brennan.io>
2020-04-05 16:17:39 -0700

    udp: Fix checksum for odd-size payload

--- kernel/udp.c
+++ kernel/udp.c
@@ -59,18 +59,21 @@ int udp_send(struct netif *netif, struct packet *pkt, uint32_t src_ip,
              uint32_t dst_ip, uint16_t src_port, uint16_t dst_port)
 {
 	uint32_t csum;
+	uint32_t len_rounded;
 	pkt->tl = pkt->al - sizeof(struct udphdr);
 
 	pkt->udp->src_port = htons(src_port);
 	pkt->udp->dst_port = htons(dst_port);
 	pkt->udp->len = htons(pkt->end - pkt->tl);
+	len_rounded = pkt->end - pkt->tl;
+	len_rounded = (len_rounded / 2) + (len_rounded % 2);
 	pkt->udp->csum = 0;
 	csum_init(&csum);
 	csum_add(&csum, &src_ip, 2);
 	csum_add(&csum, &dst_ip, 2);
 	csum_add_value(&csum, ntohs((uint16_t)IPPROTO_UDP));
 	csum_add(&csum, &pkt->udp->len, 1);
-	csum_add(&csum, (uint16_t *)pkt->udp, ntohs(pkt->udp->len) / 2);
+	csum_add(&csum, (uint16_t *)pkt->udp, len_rounded);
 	pkt->udp->csum = csum_finalize(&csum);
 	ip_send(netif, pkt, IPPROTO_UDP, src_ip, dst_ip);
 }
