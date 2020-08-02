brenns10/sos

commit 58c802e780a895beb30a853c0ff504170cd174ed

Add board support
This commit formally introduces the concept of kernel configuration, and
boards. Kernel configuration is a simple header file, and board is a
configuration option which selects some initialization routines.

With this commit, the UART base address is a kernel configuration option
as well, and its initialization sequence is modified: pre-mmu, the uart
is initialized for early puts. After mmu, the uart is remapped into
virtual memory, and once the interrupt controller is initialized, we can
turn on IRQ.

This allows us to remove knowledge of the UART from kmem_init().

kernel/uart.c
@@ -1,6 +1,7 @@
 /**
  * PL011 Driver
  */
+#include "config.h"
 #include "kernel.h"
 #include "sync.h"
 
@@ -46,7 +47,7 @@ typedef volatile struct __attribute__((packed)) {
 	uint32_t UARTDMACR;
 } pl011_registers;
 
-uint32_t uart_base = 0x09000000;
+uint32_t uart_base = CONFIG_UART_BASE;
 struct process *waiting = NULL;
 spinsem_t uart_sem;
 #define base ((pl011_registers *)uart_base)
@@ -152,22 +153,37 @@ void uart_init(void)
 {
 	uint32_t reg;
 
-	/* Half full TX/RX interrupt */
-	reg = 0x12;
-	WRITE32(base->UARTIFLS, reg);
 	/* Set 8 bit words, and enable FIFO */
-	reg = UARTLCR_FEN | UARTLCR_8BIT;
-	WRITE32(base->UARTLCR_H, reg);
+	WRITE32(base->UARTLCR_H, UARTLCR_FEN | UARTLCR_8BIT);
+
 	/* Enable UART, Tx, Rx */
-	reg = base->UARTCR;
+	reg = READ32(base->UARTCR);
 	reg |= (UARTCR_UARTEN | UARTCR_TXE | UARTCR_RXE);
 	WRITE32(base->UARTCR, reg);
-	/* Only interrupt for RX */
-	reg = UARTIMSC_UART_RXIM;
-	WRITE32(base->UARTIMSC, reg);
 
 	INIT_SPINSEM(&uart_sem, 1);
+}
+
+void uart_init_irq(void)
+{
+	/* Half full TX/RX interrupt */
+	WRITE32(base->UARTIFLS, 0x12);
+
+	/* Only interrupt for RX */
+	WRITE32(base->UARTIMSC, UARTIMSC_UART_RXIM);
 
 	gic_register_isr(UART_INTID, 1, uart_isr, "uart");
 	gic_enable_interrupt(UART_INTID);
+}
+
+/*
+ * Remap the UART base. This should be called after MMU is enabled, and we need
+ * to map a virtual address to the physical UART base.
+ */
+void uart_remap(void)
+{
+	uint32_t new_addr = alloc_pages(kern_virt_allocator, 0x1000, 0);
+	kmem_map_pages(new_addr, uart_base, 0x1000,
+	               PRW_UNA | EXECUTE_NEVER | DEVICE_SHAREABLE);
+	uart_base = new_addr;
 }

Makefile
@@ -19,7 +19,7 @@ HOSTCC = gcc
 ARCH := armv7-a
 ASFLAGS = -g -march=$(ARCH)
 CFLAGS = -g -ffreestanding -nostdlib -fPIE -march=$(ARCH) -iquote lib \
-         -iquote include -marm -Wall -Wno-address-of-packed-member
+         -iquote include -iquote kernel -marm -Wall -Wno-address-of-packed-member
 LDFLAGS = -nostdlib -fPIE
 
 TEST_CFLAGS = -fprofile-arcs -ftest-coverage -lgcov -g -DTEST_PREFIX
@@ -28,13 +28,13 @@ TEST_CFLAGS = -fprofile-arcs -ftest-coverage -lgcov -g -DTEST_PREFIX
 -include conf.mk
 
 .PHONY: run
-run: kernel.bin mydisk
+run: kernel/configvals.h kernel.bin mydisk
 	@echo Running. Exit with Ctrl-A X
 	@echo
 	$(QEMU_CMD) -kernel kernel.bin
 
 .PHONY: all
-all: kernel.bin compile_unittests
+all: kernel/configvals.h kernel.bin compile_unittests
 
 .PHONY: debug
 debug: kernel.bin mydisk
@@ -91,6 +91,9 @@ kernel.elf: lib/slab.o
 kernel.elf: lib/math.o
 kernel.elf: lib/inet.o
 
+kernel.elf: board/qemu.o
+kernel.elf: board/rpi4b.o
+
 # Object files going into each userspace program:
 USER_BASIC = user/syscall.o user/startup.o
 user/salutations.elf: user/salutations.o lib/format.o $(USER_BASIC)
@@ -146,7 +149,7 @@ unittest: compile_unittests
 	gcovr -r . --html --html-details -o cov.html lib/ unittests/
 
 .PHONY: integrationtest
-integrationtest: kernel.bin mydisk
+integrationtest: kernel/configvals.h kernel.bin mydisk
 	@QEMU_CMD="$(QEMU_CMD)" $(PYTEST) integrationtests
 
 .PHONY: testdebug
@@ -154,14 +157,32 @@ testdebug:
 	@SOS_DEBUG=true QEMU_CMD="$(QEMU_CMD) $(QEMU_DBG)" $(PYTEST) integrationtests -k $(TEST) -s
 
 .PHONY: test
-test: unittest integrationtest
+test: kernel/configvals.h unittest integrationtest
 
 .PHONY: clean
 clean:
 	rm -f *.elf *.bin
 	rm -f kernel/*.o
+	rm -f kernel/configvals.h
+	rm -f board/*.o
 	rm -f lib/*.o unittests/*.to lib/*.to
 	rm -f user/*.o user/*.elf user/*.bin
 	rm -f unittests/*.gcda unittests/*.gcno unittests/*.to unittests/*.test
 	rm -f cov.*.html
 	rm -f dump.pcap
+
+.PHONY: config_qemu config_rpi4b
+config_qemu: clean
+	cp config/qemu.h kernel/configvals.h
+config_rpi4b: clean
+	cp config/rpi4b.h kernel/configvals.h
+
+kernel/configvals.h:
+	@echo You have not configured the kernel. Please read through kernel/config.h
+	@echo and create a file kernel/configvals.h which specifies each option.
+	@echo Alternatively, select one of the following premade configurations:
+	@echo
+	@echo "  make config_qemu"
+	@echo "  make config_rpi4b"
+	@echo
+	@exit 1

board/qemu.c
@@ -1,0 +1,9 @@
+#include "config.h"
+
+#if CONFIG_BOARD == BOARD_QEMU
+
+void board_init(void)
+{
+}
+
+#endif

board/rpi4b.c
@@ -1,0 +1,9 @@
+#include "config.h"
+
+#if CONFIG_BOARD == BOARD_RPI4B
+
+void board_init(void)
+{
+}
+
+#endif

config/qemu.h
@@ -1,0 +1,2 @@
+#define CONFIG_BOARD     BOARD_QEMU
+#define CONFIG_UART_BASE 0x09000000

config/rpi4b.h
@@ -1,0 +1,2 @@
+#define CONFIG_BOARD     BOARD_RPI4B
+#define CONFIG_UART_BASE 0xFE201000

kernel/board.h
@@ -1,0 +1,6 @@
+/*
+ * board.h: Every declaration in this file must be implemented by a board.
+ */
+#pragma once
+
+void board_init(void);

kernel/config.h
@@ -1,15 +1,35 @@
 /*
  * config.h: kernel build configuration values
  *
- * There is currently no system for maintaining or enforcing configuration
- * rules, nor is there a system for getting the makefile to understand the
- * configuration values. We simply copy this header file to kernel/config.h, and
- * edit the CPP declarations.
+ * This file defines constants necessary for a kernel configuration. It then
+ * includes the user-provided configuration (configvals.h), and checks it for
+ * correctness.
+ */
+#pragma once
+
+/*
+ * Board definitions. See CONFIG_BOARD.
  */
 #define BOARD_QEMU  1
 #define BOARD_RPI4B 2
+#define _BOARD_MAX  2
+
+/* INCLUDE USER CONFIG. BELOW WE INTRODUCE EACH CONFIG VALUE AND CHECK IT. */
+#include "configvals.h"
 
 /*
- * REQUIRED: set which board we are building for
+ * CONFIG_BOARD
+ * REQUIRED: set which board we are building for.
  */
-#define CONFIG_BOARD BOARD_QEMU
+//#define CONFIG_BOARD BOARD_VALUE_OF_YOUR_CHOICE
+#if !defined(CONFIG_BOARD) || CONFIG_BOARD < 1 || CONFIG_BOARD > _BOARD_MAX
+#error "CONFIG_BOARD is not properly set"
+#endif
+
+/*
+ * CONFIG_UART_BASE
+ * REQUIRED: set the physical address of the pl011 UART
+ */
+#if !defined(CONFIG_UART_BASE)
+#error "CONFIG_UART_BASE is required"
+#endif

commit 0253e41e0b4a1e5720022e3544d7f9eaeb84307f

uart: statically declare spinsem
This is a weird one. builds.sr.ht was consistently failing. I debugged
it and determined that it got stuck in the spinlock guarding the UART
reader. I noticed the spinlock was never initialized to 1. It appears
that this happens due to the pre-mmu environment - one would assume that
the generated code to modify uart_sem was using an absolute address
(somehow) and so the wrong address got written. A bisect revealed that
the issue was introduced in 58c802e780a8 ("Add board support"), which is
the first commit to use uart_init() in the pre_mmu() environment.

The strange thing is that this only reproduces on Debian builds.sr.ht.
The main difference in that environment from mine is gcc 9.3.0 versus my
laptop suing gcc 10.1.0. qemu version is the same. This may have to do
with the generated code. Whether or note the code generated under 9.3.0
is a bug is less clear. In any case, we can simply statically initialize
this variable, which resolves the issue on the build system.

Fixes: 58c802e780a895beb30a853c0ff504170cd174ed

kernel/uart.c
@@ -49,7 +49,7 @@ typedef volatile struct __attribute__((packed)) {
 
 uint32_t uart_base = CONFIG_UART_BASE;
 struct process *waiting = NULL;
-spinsem_t uart_sem;
+DECLARE_SPINSEM(uart_sem, 1);
 #define base ((pl011_registers *)uart_base)
 
 void putc(char c)
@@ -171,8 +171,6 @@ void uart_init(void)
 	reg = READ32(base->UARTCR);
 	reg |= (UARTCR_UARTEN | UARTCR_TXE | UARTCR_RXE);
 	WRITE32(base->UARTCR, reg);
-
-	INIT_SPINSEM(&uart_sem, 1);
 }
 
 void uart_init_irq(void)