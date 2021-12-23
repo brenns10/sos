29992888fd6c3abaa5d92a53d11c829aa563690e
Stephen Brennan <stephen@brennan.io>
2021-12-23 11:38:13 -0800

    aarch64: -mgeneral-regs-only to stop using SIMD regs
    My "simple main" code crashed at printf, because the code generated by
    gcc used SIMD registers, e.g. q0. While I may be able to use those
    registers eventually, I got an undefined instruction exception. For now,
    disable the use of these registers. Hopefully I'll remove this once I
    have enabled SIMD in kernel mode.

--- arch/aarch64/config.mk
+++ arch/aarch64/config.mk
@@ -7,4 +7,5 @@ TOOLCHAIN := aarch64-linux-gnu-
 QEMU := qemu-system-aarch64 -M virt -cpu max
 
 ASFLAGS += -march=armv8-a
-CFLAGS += -march=armv8-a
+CFLAGS += -march=armv8-a -mgeneral-regs-only -DARCH=aarch64
+# TODO: remove -mgeneral-regs-only when I safely enable SIMD for kernel
